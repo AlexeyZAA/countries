8b78c58744250482f80843b6602c048d40450534
zaa <zaa@tk-barn.ru>
2020-02-13 16:26:48 +0700

    Сделано апи

--- src/store/modules/countries.js
+++ src/store/modules/countries.js
@@ -1,24 +1,29 @@
 export default {
   state: {
-    countries: [],
+    countries: null
   },
 
   getters: {
     COUNTRI: state => {
       return state.countries
-    },
+    }
   },
 
   mutations: {
     SET_COUNTRI: (state, payload) => {
       state.countries = payload
     },
+    UPDATE_COUNTRI: (state, payload) => {
+      state.countries = { ...state.params, ...payload }
+    }
   },
 
   actions: {
     SETCOUNTRI: async (context, payload) => {
       context.commit('SET_COUNTRI', payload)
+    },
+    UPDATECOUNTRI: async (context, payload) => {
+      context.commit('UPDATE_COUNTRI', payload)
     }
-
-  },
+  }
 }
--- src/views/Countri.vue
+++ src/views/Countri.vue
@@ -8,149 +8,150 @@
     </v-app-bar>
     <v-content>
       <v-container fluid>
-        <v-card class="mx-auto" max-width="80%">
-          <v-container fluid>
-            <v-row dense>
-              <!-- перебор стран в картах -->
-              <v-col
-                v-for="countr in COUNTRI"
-                :key="countr.title"
-                :cols="countr.flex"
-              >
-                <v-card>
-                  <v-img
-                    :src="countr.src"
-                    class="white--text align-end"
-                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                    height="100px"
-                  >
-                  </v-img>
-
-                  <v-card-actions>
-                    <v-spacer>Имя страны</v-spacer>
-
-                    <v-btn icon>
-                      <v-icon>mdi-pencil-box-outline</v-icon>
-                    </v-btn>
-                  </v-card-actions>
-
-                  <v-card-text class="text--primary">
-                    <div>Столица</div>
-                    <div>Регион</div>
-                    <div>Население</div>
-                  </v-card-text>
-                </v-card>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card>
-      </v-container>
-    </v-content>
-    <v-container fluid>
-      <v-data-iterator
-      :items="items"
-      :items-per-page.sync="itemsPerPage"
-      :page="page"
-      :sort-by="sortBy.toLowerCase()"
-      :sort-desc="sortDesc"
-      hide-default-footer
-    >
-      <template v-slot:default="props">
-        <v-row>
-          <v-col
-            v-for="item in props.items"
-            :key="item.name"
-            cols="12"
-            sm="3"
-            md="3"
-            lg="3"
+          <v-data-iterator
+            v-if="$store.getters.COUNTRI"
+            :items="COUNTRI"
+            :items-per-page.sync="itemsPerPage"
+            :page="page"
+            :sort-by="sortBy.toLowerCase()"
+            :sort-desc="sortDesc"
+            hide-default-footer
           >
-            <v-card>
-              <v-card-title class="subheading font-weight-bold">{{ item.name }}</v-card-title>
-              <v-list dense>
-                <v-list-item
-                  v-for="(key, index) in filteredKeys"
-                  :key="index"
+            <template v-slot:default="props">
+              <v-row>
+                <v-col
+                  v-for="item in props.items"
+                  :key="item.сountriname"
+                  :cols="3"
                 >
-                  <v-list-item-content :class="{ 'blue--text': sortBy === key }">{{ key }}:</v-list-item-content>
-                  <v-list-item-content class="align-end" :class="{ 'blue--text': sortBy === key }">{{ item[key.toLowerCase()] }}</v-list-item-content>
-                </v-list-item>
-              </v-list>
-            </v-card>
-            <v-card>
-              <v-card-title class="subheading font-weight-bold">{{ item.name }}</v-card-title>
-              <v-list dense>
-                <v-list-item
-                  v-for="(key, index) in filteredKeys"
-                  :key="index"
-                >
-                  <v-list-item-content :class="{ 'blue--text': sortBy === key }">{{ key }}:</v-list-item-content>
-                  <v-list-item-content class="align-end" :class="{ 'blue--text': sortBy === key }">{{ item[key.toLowerCase()] }}</v-list-item-content>
-                </v-list-item>
-              </v-list>
-            </v-card>
-
-          </v-col>
-        </v-row>
-      </template>
-
-      <template v-slot:footer>
-        <v-row class="mt-2" align="center" justify="center">
-          <span class="grey--text">Количество на странице</span>
-          <v-menu offset-y>
-            <template v-slot:activator="{ on }">
-              <v-btn
-                dark
-                text
-                color="primary"
-                class="ml-2"
-                v-on="on"
-              >
-                {{ itemsPerPage }}
-                <v-icon>mdi-chevron-down</v-icon>
-              </v-btn>
+                  <v-card class="mx-auto" max-width="80%">
+                    <v-img
+                      :src="item.flag"
+                      class="white--text"
+                      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                      height="160px"
+                      width="100%"
+                    >
+                    </v-img>
+                    <v-card-actions>
+                      <v-spacer
+                        ><div class="titlbold">{{ item.name }}</div></v-spacer
+                      >
+                      <v-btn icon @click="showModal(item.name)">
+                        <v-icon>mdi-pencil-box-outline</v-icon>
+                      </v-btn>
+                    </v-card-actions>
+                    <v-card-text class="text--primary">
+                      <v-simple-table>
+                        <tbody>
+                          <tr>
+                            <td class="titlbold">Столица</td>
+                            <td>{{ item.capital }}</td>
+                          </tr>
+                          <tr>
+                            <td class="titlbold">Регион</td>
+                            <td>{{ item.region }}</td>
+                          </tr>
+                          <tr>
+                            <td class="titlbold">Население</td>
+                            <td>{{ item.population }}</td>
+                          </tr>
+                        </tbody>
+                      </v-simple-table>
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
             </template>
-            <v-list>
-              <v-list-item
-                v-for="(number, index) in itemsPerPageArray"
-                :key="index"
-                @click="updateItemsPerPage(number)"
-              >
-                <v-list-item-title>{{ number }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-
-          <v-spacer></v-spacer>
-
-          <span
-            class="mr-4
-            grey--text"
-          >
-            Страница {{ page }} из {{ numberOfPages }}
-          </span>
-          <v-btn
-            fab
-            dark
-            color="blue darken-3"
-            class="mr-1"
-            @click="formerPage"
-          >
-            <v-icon>mdi-chevron-left</v-icon>
-          </v-btn>
-          <v-btn
-            fab
-            dark
-            color="blue darken-3"
-            class="ml-1"
-            @click="nextPage"
-          >
-            <v-icon>mdi-chevron-right</v-icon>
-          </v-btn>
+            <template v-slot:footer>
+              <v-row class="mt-2" align="center" justify="center">
+                <span class="grey--text">На странице</span>
+                <v-menu offset-y>
+                  <template v-slot:activator="{ on }">
+                    <v-btn dark text color="primary" class="ml-2" v-on="on">
+                      {{ itemsPerPage }}
+                      <v-icon>mdi-chevron-down</v-icon>
+                    </v-btn>
+                  </template>
+                  <v-list>
+                    <v-list-item
+                      v-for="(number, index) in itemsPerPageArray"
+                      :key="index"
+                      @click="updateItemsPerPage(number)"
+                    >
+                      <v-list-item-title>{{ number }}</v-list-item-title>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+                <v-spacer></v-spacer>
+                <span
+                  class="mr-4
+                grey--text"
+                >
+                  Страница {{ page }} из {{ numberOfPages }}
+                </span>
+                <v-btn
+                  fab
+                  dark
+                  color="blue darken-3"
+                  class="mr-1"
+                  @click="formerPage"
+                >
+                  <v-icon>mdi-chevron-left</v-icon>
+                </v-btn>
+                <v-btn
+                  fab
+                  dark
+                  color="blue darken-3"
+                  class="ml-1"
+                  @click="nextPage"
+                >
+                  <v-icon>mdi-chevron-right</v-icon>
+                </v-btn>
+              </v-row>
+            </template>
+          </v-data-iterator>
+      </v-container>
+      <template>
+        <v-row justify="center">
+          <v-dialog v-model="dialog" persistent max-width="600px">
+            <v-card>
+              <v-card-title>
+                <span class="headline">Изменение информации о стране</span>
+              </v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                  <v-col cols="12">
+                      <v-text-field label="Наименование страны*" :value="formUpdate.countriname" required></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field label="Столица" v-model="formUpdate.capital" required></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field label="Регион" v-model="formUpdate.region" required> </v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field label="Население*" v-model="formUpdate.popul" required></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+                <small>*поля обязательные для заполнения</small>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="dialog = false"
+                  >Закрыть</v-btn
+                >
+                <v-btn color="blue darken-1" text @click="saveCountriItem"
+                  >Сохранить</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </v-row>
       </template>
-    </v-data-iterator>
-    </v-container>
+    </v-content>
     <v-footer app>
       Тестовое задание
     </v-footer>
@@ -158,172 +159,162 @@
 </template>
 
 <script>
+/*
 var data = [
   {
-    сountriname: 'Favorite road trips',
+    сountriname: 'Favorite road trips1',
     src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    сountriname: 'Best airlines',
+    сountriname: 'Best airlines2',
     src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    сountriname: 'Favorite road trips',
+    сountriname: 'Favorite road trips3',
     src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    сountriname: 'Best airlines',
+    сountriname: 'Best airlines4',
     src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    сountriname: 'Favorite road trips',
+    сountriname: 'Favorite road trips5',
     src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    сountriname: 'Best airlines',
+    сountriname: 'Best airlines6',
     src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    сountriname: 'Favorite road trips',
+    сountriname: 'Favorite road trips7',
     src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    сountriname: 'Best airlines',
+    сountriname: 'Best airlines8',
     src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    сountriname: 'Favorite road trips',
+    сountriname: 'Favorite road trips9',
     src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    сountriname: 'Best airlines',
+    сountriname: 'Best airlines99',
     src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    сountriname: 'Favorite road trips',
+    сountriname: 'Favorite road trips8',
     src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    titсountrinamele: 'Best airlines',
+    titсountrinamele: 'Best airlines7',
     src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    tiсountrinametle: 'Favorite road trips',
+    tiсountrinametle: 'Favorite road trips6',
     src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    сountriname: 'Best airlines',
+    сountriname: 'Best airlines5',
     src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    сountriname: 'Favorite road trips',
+    сountriname: 'Favorite road trips4',
     src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
-    flex: 3,
+    flex: 3
   },
   {
-    сountriname: 'Best airlines',
+    сountriname: 'Best airlines3',
     src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-    flex: 3,
-  },
+    flex: 3
+  }
 ]
-
+*/
 import { mapGetters } from 'vuex'
+import axios from 'axios'
+
 export default {
   name: 'Countri',
-  data() {
+  components: {},
+  data () {
     return {
       itemsPerPageArray: [4, 8, 12],
-        search: '',
-        filter: {},
-        sortDesc: false,
-        page: 1,
-        itemsPerPage: 8,
-        sortBy: 'countriname',
-        keys: [
-          'Countriname',
-          'city',
-        ],
-        items: [
-          {
-            countriname: 'Frozen Yogurt',
-            city: 159,
-          },
-          {
-            countriname: 'Ice cream sandwich',
-            city: 237,
-          },
-          {
-            countriname: 'Eclair',
-            city: 262,
-          },
-          {
-            countriname: 'Cupcake',
-            city: 305,
-           },
-          {
-            countriname: 'Gingerbread',
-            city: 356,
-          },
-          {
-            countriname: 'Jelly bean',
-            city: 375,
-           },
-          {
-            countriname: 'Lollipop',
-            city: 392,
-          },
-          {
-            countriname: 'Honeycomb',
-            city: 408,
-          },
-          {
-            countriname: 'Donut',
-            city: 452,
-          },
-          {
-            countriname: 'KitKat',
-            city: 518,
-          },
-        ],
+      sortDesc: false,
+      page: 1,
+      itemsPerPage: 8,
+      sortBy: 'countriname',
+      keys: ['Countriname', 'capital', 'region', 'population'],
+      dialog: false,
+      formUpdate: {
+        countriname: '',
+        capital: '',
+        region: '',
+        popul: ''
+      }
     }
   },
   methods: {
-     nextPage () {
-        if (this.page + 1 <= this.numberOfPages) this.page += 1
+    showModal (name) {
+      this.dialog = true
+      this.formUpdate.countriname = name
       },
-      formerPage () {
-        if (this.page - 1 >= 1) this.page -= 1
-      },
-      updateItemsPerPage (number) {
-        this.itemsPerPage = number
-      },
+    saveCountriItem () {
+      alert('Сохранено')
+      this.formUpdate = {
+        countriname: '',
+        capital: '',
+        region: '',
+        popul: ''
+      }
+      this.dialog = false
+    },
+    getParam () {
+      const patch = 'https://restcountries.eu/rest/v2/regionalbloc/eu'
+      axios
+        .get(patch)
+        .then(response => {
+          this.$store.dispatch('SETCOUNTRI', response.data)
+        })
+        .catch(e => {
+          console.log(e)
+        })
+    },
+    nextPage () {
+      if (this.page + 1 <= this.numberOfPages) this.page += 1
+    },
+    formerPage () {
+      if (this.page - 1 >= 1) this.page -= 1
+    },
+    updateItemsPerPage (number) {
+      this.itemsPerPage = number
+    }
   },
-  mounted() {
-    this.$store.dispatch('SETCOUNTRI', data)
+  mounted () {
+    this.getParam()
   },
+  created () {},
   computed: {
     ...mapGetters(['COUNTRI']),
     numberOfPages () {
-        return Math.ceil(this.items.length / this.itemsPerPage)
-      },
-      filteredKeys () {
-        return this.keys.filter(key => key !== `Name`)
-      },
-  },
+      return Math.ceil(this.COUNTRI.length / this.itemsPerPage)
+    },
+    filteredKeys () {
+      return this.keys.filter(key => key !== `Name`)
+    }
+  }
 }
 </script>
 
@@ -337,5 +328,8 @@ export default {
   display: inline-block;
   right: 10px;
   position: absolute;
+}
+.titlbold {
+  font-weight: bold;
 }
 </style>
